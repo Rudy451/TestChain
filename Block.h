1010032760f013c5bafaa3997bece2c11bf4a1f7
Rudy451 <dane.johnson451@gmail.com>
2022-05-23 14:57:45 -0700

    mini blockchain written

---
+++ Block.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdint>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Block{
+public:
+	string sPrevHash;
+	Block(uint32_t nIndexIn, const string &sDataIn);
+	string GetHash();
+	void MineBlock(uint32_t _nDifficulty);
+private:
+	uint32_t _nIndex;
+	int64_t _nNonce;
+	string _sData;
+	string _sHash;
+	time_t _tTime;
+
+	string _CalculateHash() const;
+;
